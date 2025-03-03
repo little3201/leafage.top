87f4ee522a58457e6a6c68a5bdc6d1eb1aeabda6
wq li <little3201@163.com>
2025-03-03 12:59:20 +0800

    移除非必要配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,10 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
-import globals from "globals"
-import js from "@eslint/js"
-import pluginVue from "eslint-plugin-vue"
+
 
 export default withNuxt(
   // Your custom configs here
   [
-    {
-      languageOptions: {
-        globals: {
-          ...globals.browser,
-          ...globals.node
-        },
-        ecmaVersion: "latest",
-        sourceType: 'module'
-      }
-    },
-    js.configs.recommended,
-    ...pluginVue.configs["flat/recommended"], // Vue3
     {
       rules: {
         // https://eslint.org/docs/latest/use/configure/configuration-files
@@ -26,7 +12,6 @@ export default withNuxt(
         'vue/multi-word-component-names': 'off',
         'vue/no-multiple-template-root': 'off'
       }
-    },
-    { ignores: [".nuxt/"] }
+    }
   ]
 )
